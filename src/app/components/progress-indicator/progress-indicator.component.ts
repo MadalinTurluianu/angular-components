9a0984503371f833b98486fb1e457de009cd09cd
madalin <turluianu.petre.madalin@gmail.com>
2024-04-27 00:22:47 +0300

    almost completed, need to add "complete" output to progress indicator and add style to the app

--- src/app/components/progress-indicator/progress-indicator.component.ts
+++ src/app/components/progress-indicator/progress-indicator.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-progress-indicator',
   standalone: true,
   imports: [],
   templateUrl: './progress-indicator.component.html',
-  styleUrl: './progress-indicator.component.css'
+  styleUrl: './progress-indicator.component.css',
 })
 export class ProgressIndicatorComponent {
+  @Input() radius!: number;
+  @Input() progress!: number;
+  @Input() color!: string;
+  @Input() hollow: boolean = true;
+  @Input() showProgress: boolean = true;
 
+  @Output() complete = new EventEmitter();
+
+  ngOnInit() {
+    if (this.radius < 50) this.radius = 50;
+    if (this.progress > 100) this.progress = 100;
+    if (this.progress < 0) this.progress = 0;
+  }
 }
--- src/app/pages/progress-indicator-page/progress-indicator-page.component.ts
+++ src/app/pages/progress-indicator-page/progress-indicator-page.component.ts
@@ -1,11 +1,67 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProgressIndicatorComponent } from '../../components/progress-indicator/progress-indicator.component';
 
 @Component({
   selector: 'app-progress-indicator-page',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProgressIndicatorComponent],
   templateUrl: './progress-indicator-page.component.html',
   styleUrl: './progress-indicator-page.component.css',
 })
-export class ProgressIndicatorPageComponent {}
+export class ProgressIndicatorPageComponent {
+  input1 = {
+    color: '#4287f5',
+    progress: 30,
+    radius: 50,
+  };
+
+  input2 = {
+    color: '#4287f5',
+    progress: 60,
+    radius: 50,
+  };
+
+  input3 = {
+    color: '#4287f5',
+    progress: 81,
+    radius: 50,
+  };
+
+  updateInput1Color(event: Event) {
+    this.input1.color = (event.target as HTMLInputElement).value;
+  }
+
+  updateInput2Color(event: Event) {
+    this.input2.color = (event.target as HTMLInputElement).value;
+  }
+
+  updateInput3Color(event: Event) {
+    this.input3.color = (event.target as HTMLInputElement).value;
+    console.log(this.input3);
+  }
+
+  updateInput1Progress(event: Event) {
+    this.input1.progress = Number((event.target as HTMLInputElement).value);
+  }
+
+  updateInput2Progress(event: Event) {
+    this.input2.progress = Number((event.target as HTMLInputElement).value);
+  }
+
+  updateInput3Progress(event: Event) {
+    this.input3.progress = Number((event.target as HTMLInputElement).value);
+  }
+
+  updateInput1Radius(event: Event) {
+    this.input1.radius = Number((event.target as HTMLInputElement).value);
+  }
+
+  updateInput2Radius(event: Event) {
+    this.input2.radius = Number((event.target as HTMLInputElement).value);
+  }
+
+  updateInput3Radius(event: Event) {
+    this.input3.radius = Number((event.target as HTMLInputElement).value);
+  }
+}
