f3a918f75112a57b4499640d023811410d40514b
madalin <turluianu.petre.madalin@gmail.com>
2024-04-27 13:16:50 +0300

    added adjustments to navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { DataGridPageComponent } from './pages/data-grid-page/data-grid-page.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/data-grid', pathMatch: 'full' },
+
   {
-    path: '',
+    path: 'data-grid',
+    pathMatch: 'full',
     component: DataGridPageComponent,
   },
+
   {
     path: 'progress-indicator',
+    pathMatch: 'full',
     loadComponent: () =>
       import(
         './pages/progress-indicator-page/progress-indicator-page.component'
